bb0460693229dda2e3261d7b7e98807ab820a3b3
daniel gusko <daniel.h.gusko@gmail.com>
2026-03-03 21:35:44 +0000

    preprocess, workflow, encode

---
+++ package/preprocess.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from colorama import Fore, Style
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
